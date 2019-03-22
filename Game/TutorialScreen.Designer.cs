474fd1e56213cef53fe2836788f3951d58987b32
garrsaun253 <garrsaun253@SCS-D-360QHY>
2019-03-22 09:49:10 -0400

    Final working (no xml).

--- Game/TutorialScreen.Designer.cs
+++ Game/TutorialScreen.Designer.cs
@@ -32,8 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.playButton = new System.Windows.Forms.Button();
             this.backButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.blackBox = new System.Windows.Forms.PictureBox();
+            this.greenBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.blackBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBox)).BeginInit();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -78,32 +81,58 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
-            // pictureBox1
+            // label2
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.ErrorImage = global::Game.Properties.Resources.green;
-            this.pictureBox1.Image = global::Game.Properties.Resources.green;
-            this.pictureBox1.InitialImage = global::Game.Properties.Resources.green;
-            this.pictureBox1.Location = new System.Drawing.Point(235, 305);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(26, 28);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(195, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Press         to forfeit";
+            // 
+            // blackBox
+            // 
+            this.blackBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.blackBox.ErrorImage = global::Game.Properties.Resources.black;
+            this.blackBox.Image = global::Game.Properties.Resources.black;
+            this.blackBox.InitialImage = global::Game.Properties.Resources.black;
+            this.blackBox.Location = new System.Drawing.Point(235, 339);
+            this.blackBox.Name = "blackBox";
+            this.blackBox.Size = new System.Drawing.Size(26, 28);
+            this.blackBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.blackBox.TabIndex = 6;
+            this.blackBox.TabStop = false;
+            // 
+            // greenBox
+            // 
+            this.greenBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.greenBox.ErrorImage = global::Game.Properties.Resources.green;
+            this.greenBox.Image = global::Game.Properties.Resources.green;
+            this.greenBox.InitialImage = global::Game.Properties.Resources.green;
+            this.greenBox.Location = new System.Drawing.Point(235, 305);
+            this.greenBox.Name = "greenBox";
+            this.greenBox.Size = new System.Drawing.Size(26, 28);
+            this.greenBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.greenBox.TabIndex = 4;
+            this.greenBox.TabStop = false;
             // 
             // TutorialScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BurlyWood;
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.blackBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.greenBox);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.playButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.titleLabel);
             this.Name = "TutorialScreen";
             this.Size = new System.Drawing.Size(600, 1000);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blackBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +144,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.Button backButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox greenBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox blackBox;
     }
 }
